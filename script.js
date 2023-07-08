a1b53ddee48d9a4a5c72e30068ebbeadd27fee06
WangRaymond-Source <76501406+WangRaymond-Source@users.noreply.github.com>
2023-07-07 23:52:17 -0700

    add sticky nav bar and social bar

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ function hello(){
     console.log("hello");
     return;
 }
+//clickable script for nav bar for home page
+document.addEventListener('DOMContentLoaded', function() {
+    var navbarLinks = document.querySelectorAll('.header a');
+
+    navbarLinks.forEach(function(link) {
+      link.addEventListener('click', function(event) {
+        //prevent instant transfer to the particular section
+        event.preventDefault();
+        var target = this.getAttribute('href');
+        var targetElement = document.querySelector(target);
+        //if element exist scroll smoothly to the section of the page
+        if (targetElement) {
+          targetElement.scrollIntoView({ behavior: 'smooth' });
+        }
+      });
+    });
+});
